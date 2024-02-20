e037d704d62a134d43867d8ab3557f5035300994
mjhn010 <mjhn010@naver.com>
2024-02-20 20:02:27 +0900

    api

---
+++ main.js
@@ -0,0 +1,11 @@
+//뉴스 가져오는
+let news = []
+const apiKey = `dc89acfcece146f98594bfaabf5bc4b5`;
+const getLateNews = async()=>{
+    const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`);
+    const response = await fetch(url)
+    const data = await response.json();
+    news = data.articles;
+    console.log("dd",news);
+}
+getLateNews();
