9dd84bb55507c4c59bc38fb67b5e07f0377d4f0d
mjhn010 <mjhn010@naver.com>
2024-02-20 20:20:14 +0900

    url 수정

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 let news = []
 const apiKey = `dc89acfcece146f98594bfaabf5bc4b5`;
 const getLateNews = async()=>{
-    const url = new URL(`https://jun-newstimes.netlify.app/top-headlines?category=science`);
+    const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?category=science`);
     const response = await fetch(url)
     const data = await response.json();
     news = data.articles;
